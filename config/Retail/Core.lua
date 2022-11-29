b16c619ce79496362c7b86065dd56d7e6c5fe937
KrYo <85116320+KrYoSoftware@users.noreply.github.com>
2022-11-29 02:45:18 -0500

    Update Core.lua

--- config/Retail/Core.lua
+++ config/Retail/Core.lua
@@ -77,7 +77,7 @@ local cooldowns = { --These should be spellIDs for the spell you want to track f
 local s_count = 0;
 local btnTMWmain = nil
 function EventFrame:OnEvent(event, ...)
-
+    -- print("Welcome to Meoww Legion!")
 
     self[event](self, ...)
 end
@@ -235,14 +235,14 @@ function updateSpellCooldowns(sinceLastUpdate)
 		-- duration is the total duration of the cooldown, NOT the remaining
         local name = GetSpellInfo(spellId)
         if name~= nil then
-
+         --   print(name)
             local startTime, duration, enabled, modRate = GetSpellCooldown(name)
         if startTime  == nil  then
             CrazyFrame[spellId].t: SetColorTexture(0, 0, 0, 1)
             CrazyFrame[spellId].t:SetAllPoints(false)
                 end
         if enabled == 0 then
-
+            print(name.." is currently active, use it and wait "..duration.." seconds for the next one.");
 return 0
         elseif(startTime~= nil and duration ~= nil and startTime > 0 and duration > 0) then
             local timeRemaining=(startTime  + duration - GetTime())
@@ -298,7 +298,7 @@ end
 
 function castable(spell) 
 	local name, rank, icon, castTime, minRange, maxRange, spellID = GetSpellInfo(spell)
-
+	print(spellID)
 	local usable, nomana = IsUsableSpell(spell)
 	local cdcheck = spellcooldown(spell) == 0
 
@@ -833,7 +833,7 @@ t [1] = 0.000
 	else
 		local first = string.sub(tempString, 1, 1)
 		local secnd = string.sub(tempString, 2, string.len(tempString))
-
+		--print(secnd)
 		if first == "C" then
             modificator  = 0.008
 			button = secnd
@@ -847,8 +847,8 @@ t [1] = 0.000
 			modificator = 0.0
 			button = tempString
         end
-
-
+		--print(first)
+	--print(secnd)
 	end
 
 
@@ -1189,6 +1189,7 @@ function _CBLD.ShowWeekAuras(index, keybind)
 end
 
 function _CBLD.ShowOther(index, id)
+
 	if id == nil then
         CrazyFrame [index].t:SetColorTexture(0.0 ,0.0,0.0)
 		return
@@ -1441,18 +1442,18 @@ function _CBLD.PushDefaultData2Screen()
 	
 	if _CBLD.IsPlayerCst() == true then
         CrazyFrame [20].t:SetColorTexture(0.004 ,0.0,0.0)	
-		--print("cast")
+
 	else
 		CrazyFrame [20].t:SetColorTexture(0.0,0.0,0.0)
-		--print("no cast")
+
 	end
 	
 	if _CBLD.IsPlayerChn() == true then
         CrazyFrame [21].t:SetColorTexture(0.004 ,0.0,0.0)	
-		--print("chn")
+
 	else
 		CrazyFrame [21].t:SetColorTexture(0.0,0.0,0.0)
-		--print("no cn")
+
 	end
 	
 	-- если долго кастуем до подсветим полосу черным
